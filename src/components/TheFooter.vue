416704eaa76d4d85daf7ceb479840c10ea5d13fd
Chicony <Chicony@chicony2753@gmail.com>
2023-08-01 19:50:25 +0300

    Start project

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="footer">
+      .
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TheFooter'
+  }
+  </script>
+  
+  <style scoped>
+  .footer
+  {
+    background-color: #F8F8F8;
+    padding: 36px 78px;
+  }
+  </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader'
+}
+</script>
+
+<style scoped>
+.header
+{
+  background-color: #231F20;
+  padding: 42px 78px;
+}
+</style>
